d6b0abd8715e94197e6814040c0af337a0aaccc1
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-19 09:47:36 +0530

    Implement twoSum method in Q9.java

---
+++ Arrays/Q9.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q9 {
+    public static int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer>map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target-nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int[] ans = twoSum(arr,9);
+        System.out.println(Arrays.toString(ans));
+
+
+    }
+
+
+
+}
+
+
