a6c1ee0d92328f16e7f45db0b90b6918caf40130
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-20 14:45:01 +0530

    Add productExceptSelf method to Solution class

---
+++ Arrays/LEETCODE/288.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] right = new int[n];
+
+        int pro = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            pro = pro * nums[i];
+            right[i] = pro;
+        }
+
+        int[] ans = new int[n];
+
+        int left = 1;
+
+        for (int i = 0; i < n; i++) {
+            int val = left * (i + 1 < n ? right[i + 1] : 1);
+            ans[i] = val;
+            left = left * nums[i];
+        }
+
+        return ans;
+    }
+}
