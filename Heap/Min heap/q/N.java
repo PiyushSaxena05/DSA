352900f786dc0dd410b66a0b8d76ce429412b847
Piyush Saxena <piyushsaxena168@gmail.com>
2026-09-03 23:31:45 +0530

    Update N.java

--- Heap/Min heap/q/N.java
+++ Heap/Min heap/q/N.java
@@ -4,7 +4,16 @@ public class N {
 
     public static int klargest(int [] nums, int k){
         PriorityQueue<Integer>pq = new PriorityQueue<>();
-     
+        int largest = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            pq.offer(nums[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+
+            largest = pq.peek();
+
+        }
         return largest;
     }
     public static void main(String[] args) {
