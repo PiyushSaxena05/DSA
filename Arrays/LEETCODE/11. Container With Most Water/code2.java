4dce7e704ed88acabf6ba3b3906016f440b78183
Piyush Saxena <piyushsaxena168@gmail.com>
2026-09-03 23:33:38 +0530

    Update code2.java

--- Arrays/LEETCODE/11. Container With Most Water/code2.java
+++ Arrays/LEETCODE/11. Container With Most Water/code2.java
@@ -5,22 +5,4 @@ int srt = 0;
         while (srt < end) {
 
            
-            int minH = Math.min(height[srt], height[end]);
-
-            
-            int area = minH * (end - srt);
-
-            maxWater = Math.max(maxWater, area);
-
-            while (srt < end && height[srt] <= minH) {
-                srt++;
-            }
-
-            while (srt < end && height[end] <= minH) {
-                end--;
-            }
-        }
-
-        return maxWater;
-    
        
