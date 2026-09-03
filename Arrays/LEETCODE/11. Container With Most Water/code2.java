20ad2f90f1c17db5878558950f2f80ced3ee3f74
Piyush Saxena <piyushsaxena168@gmail.com>
2026-09-03 23:33:30 +0530

    Optimize water area calculation in code2.java
    Refactor water container calculation to use minimum height for area calculation and update pointers accordingly.

--- Arrays/LEETCODE/11. Container With Most Water/code2.java
+++ Arrays/LEETCODE/11. Container With Most Water/code2.java
@@ -5,5 +5,22 @@ int srt = 0;
         while (srt < end) {
 
            
+            int minH = Math.min(height[srt], height[end]);
+
             
+            int area = minH * (end - srt);
+
+            maxWater = Math.max(maxWater, area);
+
+            while (srt < end && height[srt] <= minH) {
+                srt++;
+            }
+
+            while (srt < end && height[end] <= minH) {
+                end--;
+            }
+        }
+
+        return maxWater;
+    
        
