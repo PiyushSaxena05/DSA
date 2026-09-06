3fc6a16a5c8c740ada4342d0dff2c7bed60a122f
Piyush Saxena <piyushsaxena168@gmail.com>
2026-09-06 13:43:11 +0530

    Implement brute force pair counting in Q11.java

---
+++ Arrays/Count Pairs/Bruteforce/Q11.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.HashMap;
+
+public class Q11 {
+    public static int countPairs(int[] arr , int target){
+
+        int count =0;
+        for (int i = 0; i < arr.length ; i++) {
+
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[i]+arr[j]==target){
+                    count++;
+                }
+
+            }
+        }
+        return count;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,5,7,-1,5};
+        int ans = countPairs(arr,6);
+        int ans2 = countPairs2(arr,6);
+        System.out.println(ans);
+        System.out.println(ans2);
+
+    }
+}
